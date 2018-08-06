dcb3ca9d1ba1ef8eb3ec4719fa2b5daf7574a0ab
soondjnf <40675551+soondjnf@users.noreply.github.com>
2018-08-06 15:27:19 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -12,6 +12,30 @@ console.log("BOT ONLINE");
 
 var prefix = "!";
 
+const adminprefix = "!";
+const devs = ['391727913415999498','463857132060540958'];
+client.on('message', message => {
+  var argresult = message.content.split(` `).slice(1).join(' ');
+    if (!devs.includes(message.author.id)) return;
+    
+if (message.content.startsWith(adminprefix + 'setgame')) {
+  client.user.setGame(argresult);
+    message.channel.sendMessage(`**${argresult} تم تغيير بلاينق البوت إلى **`)
+} else 
+  if (message.content.startsWith(adminprefix + 'setname')) {
+client.user.setUsername(argresult).then
+    message.channel.sendMessage(`**${argresult}** : تم تغيير أسم البوت إلى`)
+return message.reply("**لا يمكنك تغيير الاسم يجب عليك الانتظآر لمدة ساعتين . **");
+} else
+  if (message.content.startsWith(adminprefix + 'setavatar')) {
+client.user.setAvatar(argresult);
+  message.channel.sendMessage(`**${argresult}** : تم تغير صورة البوت`);
+      } else     
+if (message.content.startsWith(adminprefix + 'setT')) {
+  client.user.setGame(argresult, "https://www.twitch.tv/idk");
+    message.channel.sendMessage(`**تم تغيير تويتش البوت إلى  ${argresult}**`)
+}
+});
 
 
 client.on('message', message => { // Leaked by [ @! Abdulrhman#1001, @! KiNg66S.❤#8047 ]

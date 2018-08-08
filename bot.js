d681ca54f1581374436e4067a23bb90838807644
soondjnf <40675551+soondjnf@users.noreply.github.com>
2018-08-08 23:39:40 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -29,7 +29,23 @@ const adminprefix = "!";
 
 
 
+    client.on("message", message => {
+    var args = message.content.substring(prefix.length).split(" ");
+if (message.content.startsWith("مسح")){
+        if(!message.channel.guild) return message.reply(':x: اسف لكن هذا الامر للسيرفرات فقط ');
+if(!message.member.hasPermission('MANAGE_MESSAGES')) return message.reply(':warning:  لا يوجد لديك صلاحية لمسح الشات');
+var msg;
+msg = parseInt();
 
+message.channel.fetchMessages({limit: msg}).then(messages => message.channel.bulkDelete(messages)).catch(console.error);
+message.channel.sendMessage("", {embed: {
+title: "تــم مسح الشات",
+color: 0x06DF00,
+footer: "..."
+}
+})
+}
+}).then(msg => {msg.delete(3000)});
 
 client.on('message', message => {
     if (message.content.startsWith("رابط")) {
